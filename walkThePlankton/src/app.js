5542fe5d8c05617b35c443eaed22fc995b332796
mwdewey <mwdewey@optonline.net>
2015-09-16 14:13:06 -0400

    Added start screen Key bindings Keyboard enum

--- walkThePlankton/src/app.js
+++ walkThePlankton/src/app.js
@@ -1,78 +1,42 @@
-
-var HelloWorldLayer = cc.Layer.extend({
-    sprite:null,
+var StartLayer = cc.Layer.extend({
     ctor:function () {
-        //////////////////////////////
-        // 1. super init first
         this._super();
 
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
-        var size = cc.winSize;
+        cc.log("Debugging is working.");
 
-        // add a "close" icon to exit the progress. it's an autorelease object
-        var closeItem = new cc.MenuItemImage(
-            res.CloseNormal_png,
-            res.CloseSelected_png,
-            function () {
-                cc.log("Menu is clicked!");
-            }, this);
-        closeItem.attr({
-            x: size.width - 20,
-            y: 20,
-            anchorX: 0.5,
-            anchorY: 0.5
-        });
+        cc.eventManager.addListener(
+            cc.EventListener.create({
+                event: cc.EventListener.KEYBOARD,
+                onKeyPressed: this.onKeyPressed,
+                onKeyReleased: this.onKeyReleased
+            }),this);
 
-        var menu = new cc.Menu(closeItem);
-        menu.x = 0;
-        menu.y = 0;
-        this.addChild(menu, 1);
+        var startLabel = new cc.LabelTTF("Press this", "Segoe UI", 40);
+        startLabel.x = cc.winSize.width/2;
+        startLabel.y = cc.winSize.height/2;
 
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = 0;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
+        this.addChild(startLabel);
 
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2,
-            scale: 0.5,
-            rotation: 180
-        });
-        this.addChild(this.sprite, 0);
-
-        this.sprite.runAction(
-            cc.sequence(
-                cc.rotateTo(2, 0),
-                cc.scaleTo(2, 1, 1)
-            )
-        );
-        helloLabel.runAction(
-            cc.spawn(
-                cc.moveBy(2.5, cc.p(0, size.height - 40)),
-                cc.tintTo(2.5,255,125,0)
-            )
-        );
+        return true;
+    },
+    onKeyPressed:function(event){
+        if(event == enumKeyCodes.KEY_B) cc.log (event);
+        return true;
+    },
+    onKeyReleased:function (event){
+        if(event == enumKeyCodes.KEY_B) cc.log (event);
         return true;
     }
 });
 
-var HelloWorldScene = cc.Scene.extend({
+var StartScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new HelloWorldLayer();
+
+        var layer = new StartLayer();
         this.addChild(layer);
     }
+
 });
+
 
