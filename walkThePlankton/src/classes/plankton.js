05a20396c3ce95c7ab37c59a920e4b867ca2a4be
Collin Fingar <fingac@rpi.edu>
2015-09-16 17:03:10 -0400

    Added 8-way moving.

--- walkThePlankton/src/classes/plankton.js
+++ walkThePlankton/src/classes/plankton.js
@@ -8,13 +8,17 @@ var Plankton = cc.Sprite.extend({
         this._super(res.Plankton_png);
         this.x = xx;
         this.y = yy;
+        this.movingUp = false;
+        this.movingDown = false;
+        this.movingLeft = false;
+        this.movingRight = false;
         //this.moveUp = cc.moveBy(0, cc.p(0,30));
 
         cc.eventManager.addListener({
             event: cc.EventListener.KEYBOARD,
 
-            onKeyPressed:this.onKeyDown
-
+            onKeyPressed:this.onKeyDown,
+            onKeyReleased:this.onKeyUp
 
 
         }, this);
@@ -24,24 +28,66 @@ var Plankton = cc.Sprite.extend({
 
     onKeyDown:function(key, event){
         var t=event.getCurrentTarget();
+        var deltaX = 0; var deltaY = 0;
         switch(key) {
-            case enumKeyCodes.KEY_Left:
-                var moveUp = cc.moveBy(0, cc.p(-5,0));
-                t.runAction (moveUp);
-                break;
-            case enumKeyCodes.KEY_Right:
-                var moveUp = cc.moveBy(0, cc.p(5,0));
-                t.runAction (moveUp);
-                break;
+
             case enumKeyCodes.KEY_Up:
-                var moveUp = cc.moveBy(0, cc.p(0,5));
-                t.runAction (moveUp);
+                t.movingUp = true;
                 break;
             case enumKeyCodes.KEY_Down:
-                var moveUp = cc.moveBy(0, cc.p(0,-5));
-                t.runAction (moveUp);
+                t.movingDown = true;
                 break;
         }
+        switch(key){
+            case enumKeyCodes.KEY_Left:
+                t.movingLeft = true;
+                break;
+            case enumKeyCodes.KEY_Right:
+                t.movingRight = true;
+                break;
+        }
+
+        if(t.movingUp){
+            deltaY += 5;
+        }
+        if(t.movingDown){
+            deltaY -= 5;
+        }
+        if(t.movingLeft){
+            deltaX -= 5;
+        }
+        if(t.movingRight){
+            deltaX += 5;
+        }
+
+        var move = cc.moveBy(0, cc.p(deltaX, deltaY));
+        t.runAction(move);
+
+    },
+
+    onKeyUp:function(key, event){
+        var t=event.getCurrentTarget();
+        switch(key) {
+
+            case enumKeyCodes.KEY_Up:
+                t.movingUp = false;
+                break;
+            case enumKeyCodes.KEY_Down:
+                t.movingDown = false;
+                break;
+        }
+        switch(key){
+            case enumKeyCodes.KEY_Left:
+                t.movingLeft = false;
+                break;
+            case enumKeyCodes.KEY_Right:
+                t.movingRight = false;
+                break;
+        }
+    },
+
+    update:function(dt) {
+
     }
 
 
