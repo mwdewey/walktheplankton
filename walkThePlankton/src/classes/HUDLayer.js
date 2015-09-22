ee27d5fb0cf58e9c55ffe11724fdad42a1429965
mwdewey <mwdewey@optonline.net>
2015-09-22 09:59:33 -0400

    Moved HUD layer to it's own class.

--- walkThePlankton/src/classes/HUDLayer.js
+++ walkThePlankton/src/classes/HUDLayer.js
@@ -1,19 +1,24 @@
-var HUDLayer1 = cc.Layer.extend({
+var HUDLayer = cc.Layer.extend({
     ctor:function () {
         this._super();
 
-        var o1 = new cc.Sprite (res.Camera_Hud1_png);
-        var o2 = new cc.Sprite (res.Camera_Hud2_png);
-        var b1 = new cc.Sprite (res.Battery_stage1_png);
-        var b2 = new cc.Sprite (res.Battery_stage2_png);
-        var b3 = new cc.Sprite (res.Battery_stage3_png);
+        var oAnim = new cc.Sprite();
+        var o1 = new cc.SpriteFrame(res.CameraHud1_png, cc.rect(0,0,1600,900));
+        var o2 = new cc.SpriteFrame(res.CameraHud2_png, cc.rect(0,0,1600,900));
 
-        var oAnimation = new cc.RepeatForever(new cc.Animate(new cc.Animation([o1,o2], 2)));
-        var bAnimation = new cc.RepeatForever(new cc.Animate(new cc.Animation([b1,b2,b3], 2)));
+        oAnim.setPosition(cc.p(cc.winSize.width/2,cc.winSize.height/2));
+        oAnim.runAction(new cc.Animate(new cc.Animation([o1,o2], 1, 1000)));
+        this.addChild(oAnim);
 
-        //this.addChild(oAnimation);
-        //this.addChild(bAnimation);
-        
+        var bAnim = new cc.Sprite();
+        var b1 = new cc.SpriteFrame(res.BatteryStage1_png, cc.rect(0,0,1600,900));
+        var b2 = new cc.SpriteFrame(res.BatteryStage2_png, cc.rect(0,0,1600,900));
+        var b3 = new cc.SpriteFrame(res.BatteryStage3_png, cc.rect(0,0,1600,900));
+
+        bAnim.setPosition(cc.p(cc.winSize.width/2,cc.winSize.height/2));
+        bAnim.runAction(new cc.Animate(new cc.Animation([b1,b2,b3], 10, 1000)));
+        this.addChild(bAnim);
+
         return true;
     }
 });
